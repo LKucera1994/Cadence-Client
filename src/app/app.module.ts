d07afa2bbd5a46354f6281ca02565e4c434485c5
Lukas Kucera <lukaskucera94@yahoo.de>
2023-08-05 16:51:42 +0200

    added Toastr, added error interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { HttpClientModule} from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import { CoreModule } from './core/core.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { HomeModule } from './home/home.module';
+import { ErrorInterceptor } from './core/interceptors/error.interceptor';
+import { ToastrModule } from 'ngx-toastr';
 
 
 
@@ -24,11 +26,13 @@ import { HomeModule } from './home/home.module';
     NgbModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    
+    ToastrModule.forRoot(),
     AppRoutingModule,
     HomeModule    
   ],
-  providers: [],
+  providers: [
+    {provide:HTTP_INTERCEPTORS, useClass:ErrorInterceptor,multi:true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/core/test-error/test-error.component.ts
@@ -0,0 +1,42 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-test-error',
+  templateUrl: './test-error.component.html',
+  styleUrls: ['./test-error.component.css']
+})
+export class TestErrorComponent {
+  baseUrl = environment.apiUrl; 
+
+  constructor(private http: HttpClient) { }
+
+  get404Error(){
+    this.http.get(this.baseUrl +"products/100").subscribe({
+      next: response => console.log(),
+      error: error => console.error() 
+    });
+  }
+
+  get500Error(){
+    this.http.get(this.baseUrl +"buggy/servererror").subscribe({
+      next: response => console.log(),
+      error: error => console.error() 
+    });
+  }
+  get400Error(){
+    this.http.get(this.baseUrl +"buggy/badrequest").subscribe({
+      next: response => console.log(),
+      error: error => console.error() 
+    });
+  }
+  get400ValidationError(){
+    this.http.get(this.baseUrl +"product/fortytwo").subscribe({
+      next: response => console.log(),
+      error: error => console.error() 
+    });
+  }
+  
+
+}
